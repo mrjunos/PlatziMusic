d4b915404a5e3d71bac4a1c22b1df0a59b564f8c
Juanjo <jjcadu@hotmail.com>
2018-10-21 13:52:27 -0500

    Resolviendo problema de incompatibilidad https con github Pages

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import conf from "./config"
 
 export default function getArtists(country){
-	const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${conf.apiKey}&format=json`
+	const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${conf.apiKey}&format=json`
 	return fetch(URL)
 	.then(res => res.json())
 	.then(json => json.topartists.artist)
